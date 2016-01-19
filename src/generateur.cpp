2bde989f4d8345a86207591541cb9fbacd729d4d
Liciax <aboubouille@gmail.com>
2016-01-19 20:52:24 +0100

    implémentation de la méthode d'affichage

--- src/generateur.cpp
+++ src/generateur.cpp
@@ -6,6 +6,6 @@ Atom* GenAtom(int code, int action, AtomType type) {
 	p_atom->code = code;
 	p_atom->action = action;
 	p_atom->type = type;
-	p_atom->classe = CONC;
+	p_atom->classe = ATOM;
 	return p_atom;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,39 @@
 
 using namespace std;
 
+void ImprimeArbre(Node * p) {
+	int k = 1, prof = 0;
+	prof += 1;
+	for (k = 1; k <= prof; ++k)
+	{
+		cout << "---" ;
+		switch (p->classe) {
+
+			case CONC:
+				cout << "lala" <<endl;
+				break;
+			case UNION:
+				cout << "lala" <<endl;
+				break;
+			case STAR:
+				cout << "lala" <<endl;
+				break;
+			case UN:
+				cout << "lala" <<endl;
+				break;
+			case ATOM: 
+				Atom* pa = (Atom*)p;
+				cout << "> " << pa->code << endl;
+				break;
+		}
+	}
+
+}
+
 int main() {
 	
-	Atom* a = GenAtom(0,0, TERMINAL);
-
-	cout << a->code << endl;
+	Atom* a = GenAtom(1,0, TERMINAL);
+	ImprimeArbre(a);
 
 	delete(a);
 	return 0;
