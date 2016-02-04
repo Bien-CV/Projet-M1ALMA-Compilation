feef04fe239498a08715de422b59e28b93eb5946
Liciax <aboubouille@gmail.com>
2016-02-04 09:22:01 +0100

    seance de tp analyse/scan

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,95 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 #include "generateur.hpp"
 
 using namespace std;
 
-bool analyse(Node** snetf, Node* p) {
-	switch(p->classe) {
-		case CONC: {
-			if(analyse(p->left)) {
-				return analyse(p->right);
-			} else {
-				return false;
-			}
-		}
-			break;
-		case UNION: {
-			if(analyse(p->left)) {
-				return true;
-			} else {
-				return analyse(p->right);
-			}
-		}
-			break;
-		case STAR: {
-			return analyse(p->stare);
-		}
-			break;
-		case UN: {
-			return analyse(p->une);
-		}
-			break;
-		case ATOM: {
-			switch(p->classe) {
-				case TERMINAL: {
-					if(p->code == "? a confirmer") {
-						if(p->action !=0) {
-							//snetf.action(p->act)
-							// scan(string) //?
-							return true;
-						} else {
-							return false;
-						}
-					}
-				}
-					break;
-				case NONTERMINAL: {
-					if( analyse(snetf[p->code]) ) { //on choppe la case de G0 contenant l'action a effectuer 						if(p->action !=0) {
-							//snetf.action(p->act)
-							return true;
-						} 
-					} else {
-						return false;
-					}
-				}
-					break;
-			}
-		}
-			break;
-	}
-}
+// bool analyse(Node** snetf, Node* p) {
+// 	switch(p->classe) {
+// 		case CONC: {
+// 			if(analyse(p->left)) {
+// 				return analyse(p->right);
+// 			} else {
+// 				return false;
+// 			}
+// 		}
+// 			break;
+// 		case UNION: {
+// 			if(analyse(p->left)) {
+// 				return true;
+// 			} else {
+// 				return analyse(p->right);
+// 			}
+// 		}
+// 			break;
+// 		case STAR: {
+// 			return analyse(p->stare);
+// 		}
+// 			break;
+// 		case UN: {
+// 			return analyse(p->une);
+// 		}
+// 			break;
+// 		case ATOM: {
+// 			switch(p->classe) {
+// 				case TERMINAL: {
+// 					if(p->code == "? a confirmer") {
+// 						if(p->action !=0) {
+// 							//snetf.action(p->act)
+// 							// scan(string) //?
+// 							return true;
+// 						} else {
+// 							return false;
+// 						}
+// 					}
+// 				}
+// 					break;
+// 				case NONTERMINAL: {
+// 					if( analyse(snetf[p->code]) ) { //on choppe la case de G0 contenant l'action a effectuer 						if(p->action !=0) {
+// 							//snetf.action(p->act)
+// 							return true;
+// 						} 
+// 					} else {
+// 						return false;
+// 					}
+// 				}
+// 					break;
+// 			}
+// 		}
+// 			break;
+// 	}
+// }
 
 
-char scan(int nb) {
-	ifstream file(filename);
-	int k = nb;
-	string line;
-	getline(file, line);
-	while((line.length() < nb) && (!file.eof()))  {
-		nb = nb - line.length();
-		getline(file, line);
-	}
-	if(!file.eof()) {
-		return line[nb];
-	} else {
-		return '';
-	}
+string scan(int nb) {
+
+	ifstream file;
+    file.open ("src/gpl.txt");
+    string word = "";
+    char x ;
+    word.clear();
+
+    while ((!file.eof()) && (nb > 0)) {
+        x = file.get();
+        while ( x != ' ' ) {
+            word = word + x;
+            x = file.get();
+        }
+        nb--;
+        word.clear();
+    }
+    if(!file.eof()) {
+
+    	x = file.get();
+    	while ( x != ' ' ) {
+            word = word + x;
+            x = file.get();
+            cout << x;
+        }
+    }
+    return word;
 }
 
 int main() {
@@ -88,5 +102,7 @@ int main() {
 		cout << "\n";
 	}
 	
+	//cout << scan(0) << " " << scan(3) << endl; 
+
 	return 0;
 }
